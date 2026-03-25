1124bc70f7900aaafa8219c4e6b5c5a45ec037bd
PedroS42 <pedromelosantos7@gmail.com>
2026-03-25 21:32:09 +0000

    Update AI review workflow to install Ollama (local AI model) and modify yaml accordingly

--- ai_reviewer.py
+++ ai_reviewer.py
@@ -1,45 +1,34 @@
-from groq import Groq
-from groq.types.chat import ChatCompletionUserMessageParam
-import os, sys
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-API_KEY = os.environ.get("GROQ_API_KEY")
-
-if not API_KEY:
-    print("Please set GROQ_API_KEY environment variable")
-    exit(1)
-
-
-client = Groq(api_key=API_KEY)
+import ollama
+import sys
 
 with open("main.tf", "r") as file:
     code_to_review = file.read()
 
 prompt = f"Act as a Cloud security and terraform expert. Check if this code has any errors (it is meant for Azure), security vulnerabilities, invalid syntax, or bad practices. If it has, you MUST start your response with the exact word 'REJECTED'. If the code is fine and safe, start with the exact word 'APPROVED'. Then provide a brief explanation. \nTerraform Code: \n{code_to_review}"
 
-
-messages: list[ChatCompletionUserMessageParam] = [
-    {
-        "role": "user",
-        "content": prompt,
-    }
-]
 # Send the prompt to the Gemini API and get the response
-response = client.chat.completions.create(
-    messages=messages,
-    model="llama-3.3-70b-versatile"
+response = ollama.chat(
+    model="llama3.1",
+    messages=[{
+        "role": "user",
+        "content": prompt
+    }],
+    stream=True
 )
 
-print("AI Review of the Code:")
-print(response.choices[0].message.content)
-response2 = response.choices[0].message.content
+response_text = ""
 
-if response2.strip().upper().startswith("REJECTED"):
+print("AI Review of the Code:")
+for chunk in response:
+    content = chunk['message']['content']
+    print(content, end='', flush=True)
+    response_text += content
+
+print("\n")
+
+if response_text.strip().upper().startswith("REJECTED"):
     print("\nCRITICAL ERROR: AI has rejected this code! The pipeline will now stop.")
-    sys.exit(1) # this tells github: stop pipeline 
+    sys.exit(1) # this tells GitHub: stop pipeline
 else:
     print("\nSUCCESS: AI has approved this code.")
     sys.exit(0)
--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ resource "azurerm_storage_account" "meu_armazenamento" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # ERRO CRÍTICO DE SEGURANÇA:
-  allow_nested_items_to_be_public = true 
+  # Necessary so we dont allow public access to the storage account, only those with the right permissions
+  allow_nested_items_to_be_public = false
 }
